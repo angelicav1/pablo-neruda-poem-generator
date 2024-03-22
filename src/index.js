4575b80833bd7aea1dd2b164def65511e9503ed1
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-03-21 20:12:03 -0700

    added basic form handling and added typewriter js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector('#poem');
+
+  new Typewriter('#poem', {
+    strings: 'La flor mas bella',
+    autoStart: true,
+    delay: 6,
+    cursor: '🌸',
+  });
+}
+
+let poemFormElement = document.querySelector('#poem-generator-form');
+poemFormElement.addEventListener('submit', generatePoem);
