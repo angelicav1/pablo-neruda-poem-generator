f51581992f9004ea1dd892acf527c4a310b6e59f
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-03-22 19:23:13 -0700

    added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector('#poem');
-
+function displayPoem(response) {
+  console.log('Poem generated');
   new Typewriter('#poem', {
-    strings: 'La flor mas bella',
+    strings: response.data.answer,
     autoStart: true,
     delay: 6,
     cursor: '🌸',
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector('#user-instructions');
+  let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
+  let context =
+    'You are an expert in Pablo Neruda poetry and love to write short romantic poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with "Inspired by love" inside a <stron></strong> element at the end of the poem and NOT at the beginning';
+  let prompt = `User instructions: Generate a poem by Pablo Neruda about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log('Generating poem...');
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector('#poem-generator-form');
